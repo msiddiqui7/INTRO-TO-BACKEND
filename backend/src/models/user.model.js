36603418b62edf450795c7649ecb8878352e5800
Irfan Siddiqui <msiddiqui7@hotmail.com>
2026-04-12 18:39:28 -0700

    Able to succesfully register a user with the following changes:
    user.model.js — Defines the user schema and model for MongoDB.
    
    user.route.js — Sets up the registration route and connects it to the controller.
    
    user.controller.js — Contains the logic for handling registration (validation, checking for existing users, saving new users).
    
    app.js — Integrates the routes into your Express application and sets up middleware.

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,4 @@
 import mongoose, {Schema} from "mongoose";
-//import { use } from "react";
 
 const userSchema = new Schema(
   {
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import {registerUser} from "../controller/user.controller.js";
+
 const router = Router();
 
 router.route("/register").post(registerUser);
 //Wht is router.route("/register").post(registerUser);?
 //This line defines a route for handling POST requests to the "/register" endpoint. When a client sends a POST request to this endpoint, the registerUser function from the user.controller.js file will be executed to handle the registration logic for a new user. The router.route() method allows you to chain multiple HTTP methods (like GET, POST, etc.) for the same route, making it easier to manage different actions for the same endpoint.
+//example route: http://localhost:5000/api/v1/users/register/
 
 export default router;
